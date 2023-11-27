7e6e983d518941e20a951acf0047d9dc6a733298
Kiss-Meirosu László József <laszlokissmeirosu@gmail.com>
2023-11-27 11:04:29 +0100

    restructured

---
+++ cell/Cell.java
@@ -0,0 +1,29 @@
+package cell;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Cell extends JButton{
+    private boolean status;
+    Color alive = Color.BLACK;
+    Color dead = Color.WHITE;
+
+    public Cell(){
+        status = false;
+        setBackground(dead);
+    }
+
+    public void changeStatus(){
+        status = !status;
+        setBackground(status ? alive : dead);
+    }
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public void setStatus(boolean st) {
+        status = st;
+        setBackground(status ? alive : dead);
+    }
+}
---
+++ main/Main.java
@@ -0,0 +1,135 @@
+package main;
+
+import javax.swing.*;
+
+import grid.Grid;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+public class Main extends JFrame {
+    private String born="3";
+    private String survive="2;3";
+    private int size=50;
+    Grid g;
+
+    public Main(){
+        setTitle("Sejtautomata");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //JPanel menu = new JPanel();
+        JButton startButton = new JButton("Inditas");
+        JButton exitButton = new JButton("Kilepes");
+        JButton loadButton = new JButton("Betoltes");
+        JButton randomButton = new JButton("Inditas veletlenszeru alaphelyzettel");
+        final JTextField bornTextField = new JTextField(5);
+        final JTextField surviveTextField = new JTextField(5);
+        final JTextField sizeTextField = new JTextField(5);
+        JLabel bornLabel = new JLabel("Szuleteshez szukseges");
+        JLabel surviveLabel = new JLabel("Tuleleshez szukseges");
+        JLabel sizeLabel = new JLabel("Meret");
+        JPanel fields = new JPanel();
+        JPanel botButtonsPanel = new JPanel(new BorderLayout());
+        JPanel topButtonsPanel = new JPanel(new BorderLayout());
+        botButtonsPanel.add(loadButton,BorderLayout.NORTH);
+        botButtonsPanel.add(exitButton,BorderLayout.SOUTH);
+        topButtonsPanel.add(randomButton,BorderLayout.SOUTH);
+        topButtonsPanel.add(startButton,BorderLayout.NORTH);
+        fields.add(bornLabel);
+        fields.add(bornTextField);
+        fields.add(surviveLabel);
+        fields.add(surviveTextField);
+        fields.add(sizeLabel);
+        fields.add(sizeTextField);
+        add(topButtonsPanel,BorderLayout.NORTH);
+        add(fields,BorderLayout.CENTER);
+        add(botButtonsPanel,BorderLayout.SOUTH);
+
+        bornTextField.setText(born+"");
+        surviveTextField.setText(survive+"");
+        sizeTextField.setText(size+"");
+
+
+        exitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+            }
+        });
+        
+        
+        loadButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                File input=new File("a.ser");
+		        ObjectInputStream o=null;
+		        try {
+			        o = new ObjectInputStream(new FileInputStream(input));
+			        g = (Grid)o.readObject();
+                    //g.updateGrid();
+                    g.run();
+                    o.close();
+		        }catch(IOException | ClassNotFoundException err ) {throw new RuntimeException(err);}
+                
+            }
+        });
+        
+        
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                setBorn(bornTextField.getText());
+                setSurvive(surviveTextField.getText());
+                if(sizeTextField.getText()!=null) setSize(Integer.parseInt(sizeTextField.getText()));
+                
+                g = new Grid(born, survive, size);
+                
+
+            }
+        });
+
+        randomButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                setBorn(bornTextField.getText());
+                setSurvive(surviveTextField.getText());
+                if(sizeTextField.getText()!=null) setSize(Integer.parseInt(sizeTextField.getText()));
+                
+                g = new Grid(born, survive, size);
+                g.random();
+                
+
+            }
+        });
+
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void setBorn(String born) {
+        this.born = born;
+    }
+    
+    public void setSurvive(String survive) {
+        this.survive = survive;
+    }
+
+    public String getSurvive() {
+        return survive;
+    }
+
+    public String getBorn() {
+        return born;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public static void main(String[] args){
+        new Main();
+    }
+}
+
